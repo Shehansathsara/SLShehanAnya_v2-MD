20ce2e2525edc5065df7b775d966b9d795348540
Shehansathsara <138267143+Shehansathsara@users.noreply.github.com>
2023-07-02 07:30:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,25 +23,25 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 
 
 global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
+global.author = "Shehanසුද්දා"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.footer = "©\t" + "Shehanසුද්දා"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "Shehanසුද්දා"    
+global.ownernumber = ["94753382972"]
+global.ytname = "YT: Shehanසුද්දා"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.region = "Sri Lanka"
+global.state = "Colombo" // your state name
+global.timezone = "Asia/Colombo" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.email = "shehansathsarafreefire@gmail.com"
 
 
 // Greeting messages
